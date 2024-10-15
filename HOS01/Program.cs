7272019db6dac519de9477e7b22f6227ffaffb48
Krystal <loveyogawithkrystal@gmail.com>
2024-10-14 21:39:01 -0500

    Add project files.

---
+++ HOS01/Program.cs
@@ -0,0 +1,34 @@
+﻿// traditional first program written.
+// HOS01 - 
+
+using System;
+using static System.Console;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            //Console.WriteLine("      /|");
+            //Console.WriteLine("     / |");
+            //Console.WriteLine("    /  |");
+            //Console.WriteLine("   /   |");
+            //Console.WriteLine("  /    |");
+            //Console.WriteLine(" /     |");
+            //Console.WriteLine("/______|");
+            string Name = "Krystal Salinas";
+            int Age = 37;
+            bool isFemale = true;
+            float MyFloat = 10.1f;
+
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);     
+            Console.WriteLine(isFemale);
+            Console.WriteLine(MyFloat);
+        }
+
+    }
+}
+
